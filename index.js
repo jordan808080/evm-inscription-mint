c3a65dfcff924eb2321cf0647ca91f3c833523c4
jordanzhao82 <jordanzhao82@gmail.com>
2023-12-18 16:38:18 +0800

    aa

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function getGasLimit(hexData, address) {
 
 // 转账交易
 async function sendTransaction(nonce) {
-  const hexData	= convertToHexa(config.tokenJson.trim());
+  const hexData = convertToHexa(config.tokenJson.trim());
   // 获取实时 gasPrice
   const currentGasPrice = await getGasPrice();
   // 在当前 gasPrice 上增加 一定倍数
@@ -71,7 +71,7 @@ async function sendTransaction(nonce) {
 
   const transaction = {
     to: address,
-	// 替换为你要转账的金额
+  // 替换为你要转账的金额
     value: ethers.utils.parseEther(payPrice),
     // 十六进制数据
     data: hexData,
@@ -79,63 +79,28 @@ async function sendTransaction(nonce) {
     nonce: nonce,
     // 设置 gas 价格
     gasPrice: increasedGasPrice,
-	// 限制gasLimit，根据当前网络转账的设置，不知道设置多少的去区块浏览器看别人转账成功的是多少
+  // 限制gasLimit，根据当前网络转账的设置，不知道设置多少的去区块浏览器看别人转账成功的是多少
     gasLimit: gasLimit,
   };
 
   try {
     const tx = await wallet.sendTransaction(transaction);
     console.log(`Transaction with nonce ${nonce} hash:`, tx.hash);
-    return tx.hash;
   } catch (error) {
     console.error(`Error in transaction with nonce ${nonce}:`, error.message);
-    return null;
   }
-}
-const interval = 30; // 秒  
-  
-async function checkTransaction(targetTransactionHash, intervalInSeconds) {
-  async function pollTransaction() {
-    try {
-      // 使用以太坊节点提供商查询交易信息
-      const transaction = await provider.getTransaction(targetTransactionHash);
-
-      if (transaction) {
-        console.log('交易存在');
-        // 如果存在交易，你可以在这里执行你的逻辑
-        clearInterval(intervalId);
-      } else {
-        console.log('交易不存在');
-      }
-    } catch (error) {
-      console.error('发生错误：', error);
-    }
-  }
-
-  // 设置定时器每隔一段时间执行一次查询
-  const intervalId = setInterval(pollTransaction, intervalInSeconds * 1000);
-
-  // 初始立即执行一次查询
-  await pollTransaction();
 }
 
 // 发送多次交易
 async function sendTransactions() {
   const currentNonce = await getCurrentNonce(wallet);
   const sleepTime = config.sleepTime
-   // const gasPrice = await getGasPrice();
-   //  const txHash = await sendTransaction(currentNonce + 1, gasPrice);
-   //  console.log(`txHash: ${txHash}`);
-   //  await loopUntilFound(txHash);
-  
+
   for (let i = 0; i < config.repeatCount; i++) {
     const gasPrice = await getGasPrice();
-    const txHash = await sendTransaction(currentNonce + i, gasPrice);
-    await checkTransaction(txHash,interval);
-    console.log(`success`);
+    await sendTransaction(currentNonce + i, gasPrice);
     await sleep(sleepTime)
   }
-  
 }
 
 sendTransactions();
